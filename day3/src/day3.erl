c0198e68052e1221b5656d548d46b137109c7271
Harry Schneider <Harry.g.schneider@gmail.com>
2019-12-18 23:18:00 +0000

    Started working on day3 part 2, will continue at work tomorrow

--- day3/src/day3.erl
+++ day3/src/day3.erl
@@ -4,14 +4,17 @@
 %%%-------------------------------------------------------------------
 
 -module(day3).
--export([start/0, get_closes_cross/1, get_all_cross_points/2, get_all_points/1, get_points_from_move/2, readfile/1]).
+-export([start/0, get_closes_cross/1, get_all_cross_points/2, get_all_points/1, get_points_from_move/3, readfile/1]).
 
 start() ->
     get_closes_cross(readfile(input)).
 
+get_lowest_step_cross(Line1, Line2) ->
+    test.
+
 get_closes_cross(Lines) ->
     {Line1, Line2} = Lines,
-    lists:min(lists:map(fun({X,Y}) -> abs(X) + abs(Y) end, get_all_cross_points(Line1, Line2))).
+    lists:min(lists:map(fun({X,Y,_}) -> abs(X) + abs(Y) end, get_all_cross_points(Line1, Line2))).
 
 get_all_cross_points(Line1, Line2) ->
     Line1Points = get_all_points(Line1),
@@ -19,25 +22,26 @@ get_all_cross_points(Line1, Line2) ->
     lists:filter(fun(Elm) -> lists:member(Elm, Line2Points) end, Line1Points).
 
 get_all_points(Line) ->
-    get_all_points(Line, [], {0,0}).
-get_all_points([], Points, _) ->
+    get_all_points(Line, [], {0,0}, 0).
+get_all_points([], Points, _, _) ->
     Points;
-get_all_points([H|T], Points, Pos) ->
-    [NewPos|NewPoints] = get_points_from_move(H, Pos),
-    get_all_points(T, [NewPos|NewPoints] ++ Points, NewPos).
+get_all_points([H|T], Points, Pos, StepCount) ->
+    {[NewPos|NewPoints], NewStepCount} = get_points_from_move(H, Pos, StepCount),
+    get_all_points(T, [NewPos|NewPoints] ++ Points, NewPos, NewStepCount).
     
-get_points_from_move(Move, Pos) ->
-    get_points_from_move(string:sub_string(Move,1,1), erlang:list_to_integer(lists:nthtail(1,Move)), Pos, []).
-get_points_from_move(_, 0, _, List) ->
-    List;
-get_points_from_move(Direction, Amount, {X,Y}, List) ->
+get_points_from_move(Move, Pos, StepCount) ->
+    get_points_from_move(string:sub_string(Move,1,1), erlang:list_to_integer(lists:nthtail(1,Move)), Pos, StepCount, []).
+get_points_from_move(_,0,_,StepCount,List) ->
+    {List,StepCount};
+get_points_from_move(Direction, Amount, {X,Y}, StepCount, List) ->
     case Direction of
         "R" -> NewPos = {X + 1, Y};
         "L" -> NewPos = {X - 1, Y};
         "U" -> NewPos = {X, Y + 1};
         "D" -> NewPos = {X, Y - 1}
     end,
-    get_points_from_move(Direction, Amount - 1, NewPos, [NewPos|List]).
+    {NewX,NewY} = NewPos,
+    get_points_from_move(Direction, Amount - 1, NewPos, StepCount + 1, [{NewX,NewY,StepCount}|List]).
 
 readfile(FileName) ->
   {ok, Binary} = file:read_file(FileName),
--- day3/test/day3_test.erl
+++ day3/test/day3_test.erl
@@ -2,6 +2,12 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
+get_lowest_step_cross_1_test() ->
+    ?assertEqual(610, day3:get_lowest_step_cross(["R75","D30","R83","U83","L12","D49","R71","U7","L72"],["U62","R66","U55","R34","D71","R55","D58","R83"])).
+
+get_lowest_step_cross_2_test() ->
+    ?assertEqual(410, day3:get_lowest_step_cross(["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"])).
+
 get_closes_cross_1_test() ->
     ?assertEqual(159, day3:get_closes_cross(
         {["R75","D30","R83","U83","L12","D49","R71","U7","L72"],
@@ -20,7 +26,7 @@ get_all_points_test() ->
     ?assertEqual([{2,3},{2,4},{2,5},{3,5},{3,4},{3,3},{3,2},{3,1},{3,0},{2,0},{1,0}], day3:get_all_points(["R3","U5","L1","D2"])).
 
 get_points_from_move_right_test() ->
-    ?assertEqual([{6,5},{5,5},{4,5}], day3:get_points_from_move("R3", {3,5})).
+    ?assertEqual([{6,5,2},{5,5,1},{4,5,0}], day3:get_points_from_move("R3", {3,5},0)).
 get_points_from_move_left_test() ->
     ?assertEqual([{-3,4},{-2,4},{-1,4},{0,4}], day3:get_points_from_move("L4", {1,4})).
 get_points_from_move_up_test() ->
