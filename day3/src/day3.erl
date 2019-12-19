d712c11407526722693b7ef95960cf69d3bcef87
Harry <Harry.Schneider@AquilaHeywood.co.uk>
2019-12-19 09:55:50 +0000

    Finished day3 part 2.

--- day3/src/day3.erl
+++ day3/src/day3.erl
@@ -4,44 +4,52 @@
 %%%-------------------------------------------------------------------
 
 -module(day3).
--export([start/0, get_closes_cross/1, get_all_cross_points/2, get_all_points/1, get_points_from_move/3, readfile/1]).
+-export([start/0, get_lowest_step_cross/2, get_all_cross_points/2, get_all_points/1, get_points_from_move/2, readfile/1]).
 
 start() ->
-    get_closes_cross(readfile(input)).
+    {Line1, Line2} = readfile(input),
+    get_lowest_step_cross(Line1, Line2).
 
 get_lowest_step_cross(Line1, Line2) ->
-    test.
+    lists:min(lists:map(fun({_,StepCount}) -> StepCount end, get_all_cross_points(Line1, Line2))).
 
-get_closes_cross(Lines) ->
-    {Line1, Line2} = Lines,
-    lists:min(lists:map(fun({X,Y,_}) -> abs(X) + abs(Y) end, get_all_cross_points(Line1, Line2))).
+%get_closes_cross(Lines) ->
+%    {Line1, Line2} = Lines,
+%    lists:min(lists:map(fun({X,Y,_}) -> abs(X) + abs(Y) end, get_all_cross_points(Line1, Line2))).
 
 get_all_cross_points(Line1, Line2) ->
-    Line1Points = get_all_points(Line1),
-    Line2Points = get_all_points(Line2),
-    lists:filter(fun(Elm) -> lists:member(Elm, Line2Points) end, Line1Points).
+    Line1Points = lists:reverse(get_all_points(Line1)),
+    Line2Points = lists:reverse(get_all_points(Line2)),
+    Line1CoordinatesSet = sets:from_list(lists:map(fun({Coordinates,_}) -> Coordinates end, Line1Points)),
+    Line2CoordinatesSet = sets:from_list(lists:map(fun({Coordinates,_}) -> Coordinates end, Line2Points)),
+    CrossCoordinates = sets:to_list(sets:intersection(Line1CoordinatesSet, Line2CoordinatesSet)),
+    lists:map(fun(Coordinate) -> {Coordinate, get_first_occurrence(Coordinate, Line1Points) + get_first_occurrence(Coordinate, Line2Points)} end, CrossCoordinates).
+
+get_first_occurrence(Coordinate, [{Coordinate,StepCount}|_]) ->
+    StepCount;
+get_first_occurrence(Coordinate, [_|T]) ->
+    get_first_occurrence(Coordinate, T).
 
 get_all_points(Line) ->
-    get_all_points(Line, [], {0,0}, 0).
-get_all_points([], Points, _, _) ->
+    get_all_points(Line, [], {{0,0},0}).
+get_all_points([], Points, _) ->
     Points;
-get_all_points([H|T], Points, Pos, StepCount) ->
-    {[NewPos|NewPoints], NewStepCount} = get_points_from_move(H, Pos, StepCount),
-    get_all_points(T, [NewPos|NewPoints] ++ Points, NewPos, NewStepCount).
+get_all_points([H|T], Points, Pos) ->
+    [NewPos|NewPoints] = get_points_from_move(H, Pos),
+    get_all_points(T, [NewPos|NewPoints] ++ Points, NewPos).
     
-get_points_from_move(Move, Pos, StepCount) ->
-    get_points_from_move(string:sub_string(Move,1,1), erlang:list_to_integer(lists:nthtail(1,Move)), Pos, StepCount, []).
-get_points_from_move(_,0,_,StepCount,List) ->
-    {List,StepCount};
-get_points_from_move(Direction, Amount, {X,Y}, StepCount, List) ->
+get_points_from_move(Move, Pos) ->
+    get_points_from_move(string:sub_string(Move,1,1), erlang:list_to_integer(lists:nthtail(1,Move)), Pos, []).
+get_points_from_move(_,0,_,List) ->
+    List;
+get_points_from_move(Direction, Amount, {{X,Y},StepCount}, List) ->
     case Direction of
-        "R" -> NewPos = {X + 1, Y};
-        "L" -> NewPos = {X - 1, Y};
-        "U" -> NewPos = {X, Y + 1};
-        "D" -> NewPos = {X, Y - 1}
+        "R" -> NewPos = {{X + 1, Y}, StepCount + 1};
+        "L" -> NewPos = {{X - 1, Y}, StepCount + 1};
+        "U" -> NewPos = {{X, Y + 1}, StepCount + 1};
+        "D" -> NewPos = {{X, Y - 1}, StepCount + 1}
     end,
-    {NewX,NewY} = NewPos,
-    get_points_from_move(Direction, Amount - 1, NewPos, StepCount + 1, [{NewX,NewY,StepCount}|List]).
+    get_points_from_move(Direction, Amount - 1, NewPos, [NewPos|List]).
 
 readfile(FileName) ->
   {ok, Binary} = file:read_file(FileName),
--- day3/test/day3_test.erl
+++ day3/test/day3_test.erl
@@ -3,33 +3,37 @@
 
 
 get_lowest_step_cross_1_test() ->
-    ?assertEqual(610, day3:get_lowest_step_cross(["R75","D30","R83","U83","L12","D49","R71","U7","L72"],["U62","R66","U55","R34","D71","R55","D58","R83"])).
+    ?assertEqual(610, day3:get_lowest_step_cross(
+        ["R75","D30","R83","U83","L12","D49","R71","U7","L72"],
+        ["U62","R66","U55","R34","D71","R55","D58","R83"])).
 
 get_lowest_step_cross_2_test() ->
-    ?assertEqual(410, day3:get_lowest_step_cross(["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"])).
+    ?assertEqual(410, day3:get_lowest_step_cross(
+        ["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],
+        ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"])).
 
-get_closes_cross_1_test() ->
-    ?assertEqual(159, day3:get_closes_cross(
-        {["R75","D30","R83","U83","L12","D49","R71","U7","L72"],
-        ["U62","R66","U55","R34","D71","R55","D58","R83"]})).
+%get_closes_cross_1_test() ->
+%    ?assertEqual(159, day3:get_closes_cross(
+%        {["R75","D30","R83","U83","L12","D49","R71","U7","L72"],
+%        ["U62","R66","U55","R34","D71","R55","D58","R83"]})).
 
-get_closes_cross_2_test() ->
-    ?assertEqual(135, day3:get_closes_cross(
-        {["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]})).
+%get_closes_cross_2_test() ->
+%    ?assertEqual(135, day3:get_closes_cross(
+%        {["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]})).
 
 get_all_cross_points_1_test() ->
-    ?assertEqual([{2,2},{1,1}], day3:get_all_cross_points(["U1","R2","U1"], ["R1","U2","R1"])).
+    ?assertEqual([{{1,1},4},{{2,2},8}], day3:get_all_cross_points(["U1","R2","U1"], ["R1","U2","R1"])).
 get_all_cross_points_2_test() ->
-    ?assertEqual([{3,-4},{2,-1}], day3:get_all_cross_points(["D1","R3","D7"], ["R2","D4","R6"])).
+    ?assertEqual([{{2,-1},6},{{3,-4},14}], day3:get_all_cross_points(["D1","R3","D7"], ["R2","D4","R6"])).
 
 get_all_points_test() ->
-    ?assertEqual([{2,3},{2,4},{2,5},{3,5},{3,4},{3,3},{3,2},{3,1},{3,0},{2,0},{1,0}], day3:get_all_points(["R3","U5","L1","D2"])).
+    ?assertEqual([{{2,3},11},{{2,4},10},{{2,5},9},{{3,5},8},{{3,4},7},{{3,3},6},{{3,2},5},{{3,1},4},{{3,0},3},{{2,0},2},{{1,0},1}], day3:get_all_points(["R3","U5","L1","D2"])).
 
 get_points_from_move_right_test() ->
-    ?assertEqual([{6,5,2},{5,5,1},{4,5,0}], day3:get_points_from_move("R3", {3,5},0)).
+    ?assertEqual([{{3,0},3},{{2,0},2},{{1,0},1}], day3:get_points_from_move("R3", {{0,0},0})).
 get_points_from_move_left_test() ->
-    ?assertEqual([{-3,4},{-2,4},{-1,4},{0,4}], day3:get_points_from_move("L4", {1,4})).
+    ?assertEqual([{{-3,4},7},{{-2,4},6},{{-1,4},5},{{0,4},4}], day3:get_points_from_move("L4", {{1,4},3})).
 get_points_from_move_up_test() ->
-    ?assertEqual([{3,6}], day3:get_points_from_move("U1", {3,5})).
+    ?assertEqual([{{3,6},4}], day3:get_points_from_move("U1", {{3,5},3})).
 get_points_from_move_down_test() ->
-    ?assertEqual([{8,-5},{8,-4}], day3:get_points_from_move("D2", {8,-3})).
+    ?assertEqual([{{8,-5},22},{{8,-4},21}], day3:get_points_from_move("D2", {{8,-3},20})).
